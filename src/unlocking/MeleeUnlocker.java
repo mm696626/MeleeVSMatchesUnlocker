ecc5e8971a75b90f8e783b64cfeb806ea9044ff8
mm696626 <mmccullough6899@gmail.com>
2024-06-09 15:37:37 -0400

    Added delay to all 0 delay button presses or rapid presses since I was having issues on Windows

--- src/unlocking/MeleeUnlocker.java
+++ src/unlocking/MeleeUnlocker.java
@@ -23,53 +23,55 @@ public class MeleeUnlocker extends JFrame {
     private void unlock(int[] buttonAssignments) throws AWTException, InterruptedException {
         Robot robot = new Robot();
 
-        JOptionPane.showMessageDialog(this, "Found unlockable at " + vsMatchesTarget + " VS matches!" + " Boot Melee and click into your window within 2 seconds of closing this box");
+        JOptionPane.showMessageDialog(this, "Found unlockable at " + vsMatchesTarget + " VS matches!" + " Boot Melee and go to the main menu (hover over the first option in the menu) and click into your window within 2 seconds after pressing OK on this box");
         Thread.sleep(2000);
 
         //press down to go to Melee mode
         pressKey(robot, 200, buttonAssignments[ButtonConstants.DOWN_ON_STICK]);
-        simulateLoadTime(200);
+        simulateWaitOrLoadTime(200);
 
         //press A twice
         for (int i=0; i<2; i++) {
             pressKey(robot, 200, buttonAssignments[ButtonConstants.A]);
+            simulateWaitOrLoadTime(100);
         }
 
         //simulate loading time
-        simulateLoadTime(5000);
+        simulateWaitOrLoadTime(5000);
 
         //Pick Fox
         pressKey(robot, 100, buttonAssignments[ButtonConstants.RIGHT_ON_STICK]);
         pressKey(robot, 450, buttonAssignments[ButtonConstants.UP_ON_STICK]);
-        pressKey(robot, 0, buttonAssignments[ButtonConstants.A]);
+        pressKey(robot, 100, buttonAssignments[ButtonConstants.A]);
 
         //go to rules
         pressKey(robot, 200, buttonAssignments[ButtonConstants.UP_ON_STICK]);
         pressKey(robot, 100, buttonAssignments[ButtonConstants.RIGHT_ON_STICK]);
-        pressKey(robot, 0, buttonAssignments[ButtonConstants.A]);
+        pressKey(robot, 100, buttonAssignments[ButtonConstants.A]);
 
-        simulateLoadTime(500);
+        simulateWaitOrLoadTime(500);
 
         //press right to change to stock
-        pressKey(robot, 0, buttonAssignments[ButtonConstants.RIGHT_ON_STICK]);
+        pressKey(robot, 100, buttonAssignments[ButtonConstants.RIGHT_ON_STICK]);
 
-        simulateLoadTime(1000);
-        pressKey(robot, 0, buttonAssignments[ButtonConstants.DOWN_ON_STICK]);
-        pressKey(robot, 0, buttonAssignments[ButtonConstants.LEFT_ON_STICK]);
-        pressKey(robot, 0, buttonAssignments[ButtonConstants.LEFT_ON_STICK]);
-        pressKey(robot, 0, buttonAssignments[ButtonConstants.B]);
+        simulateWaitOrLoadTime(1000);
+        pressKey(robot, 100, buttonAssignments[ButtonConstants.DOWN_ON_STICK]);
+        pressKey(robot, 100, buttonAssignments[ButtonConstants.LEFT_ON_STICK]);
+        simulateWaitOrLoadTime(100);
+        pressKey(robot, 100, buttonAssignments[ButtonConstants.LEFT_ON_STICK]);
+        pressKey(robot, 100, buttonAssignments[ButtonConstants.B]);
 
 
         //Turn on a CPU
         pressKey(robot, 180, buttonAssignments[ButtonConstants.RIGHT_ON_STICK]);
         pressKey(robot, 250, buttonAssignments[ButtonConstants.UP_ON_STICK]);
-        pressKey(robot, 0, buttonAssignments[ButtonConstants.A]);
-        simulateLoadTime(200);
+        pressKey(robot, 100, buttonAssignments[ButtonConstants.A]);
+        simulateWaitOrLoadTime(200);
 
         //start loop of VS matches
         for (int i=vsMatches; i<vsMatchesTarget; i++) {
             doVSMatch(robot);
-            simulateLoadTime(4000);
+            simulateWaitOrLoadTime(4000);
 
             //press start to go to stage select after match
             pressKey(robot, 200, buttonAssignments[ButtonConstants.START]);
@@ -92,25 +94,25 @@ public class MeleeUnlocker extends JFrame {
         Thread.sleep(5000);
 
         //dash to the left to SD
-        pressKey(robot, 0, buttonAssignments[ButtonConstants.LEFT_ON_STICK]);
-        simulateLoadTime(50);
+        pressKey(robot, 50, buttonAssignments[ButtonConstants.LEFT_ON_STICK]);
+        simulateWaitOrLoadTime(50);
         pressKey(robot, 2000, buttonAssignments[ButtonConstants.LEFT_ON_STICK]);
 
         //wait for results screen to end and mash Start
-        simulateLoadTime(8500);
+        simulateWaitOrLoadTime(8500); //this could be shorter, but I wanted to guarantee the victory animation would finish
         pressKey(robot, 200, buttonAssignments[ButtonConstants.START]);
         Thread.sleep(500);
         pressKey(robot, 200, buttonAssignments[ButtonConstants.START]);
     }
 
-    private void simulateLoadTime(int duration) throws InterruptedException {
+    private void simulateWaitOrLoadTime(int duration) throws InterruptedException {
         Thread.sleep(duration);
     }
 
     private void pickMushroomKingdom(Robot robot) throws InterruptedException {
         pressKey(robot, 250, buttonAssignments[ButtonConstants.RIGHT_ON_STICK]);
         pressKey(robot, 180, buttonAssignments[ButtonConstants.UP_ON_STICK]);
-        pressKey(robot, 0, buttonAssignments[ButtonConstants.A]);
+        pressKey(robot, 100, buttonAssignments[ButtonConstants.A]);
     }
 
     private void pressKey(Robot robot, int duration, int keyCode) throws InterruptedException {
